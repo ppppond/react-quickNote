51b676f1fc32f6f85d54006ce057358258c52da8
Pond_thanabordee <thanabordeesu1@gmail.com>
2026-05-09 22:31:24 +0700

    create function toggle finish display

--- src/component/NoteItem.jsx
+++ src/component/NoteItem.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
 function NoteItem({ note, deleteNote }) {
+    const [ finish, setFinish ] = useState(false);
+
+    // สามารถกำหนดค่าใน useState แบบตรงข้ามได้เลย "!"
+    const toggleFinish = () => setFinish(!finsih)
 
     return (
         <div className="note-container">
-            <h2>{note.text}</h2>
-            <button className="finish">finish</button>
+            <h2 style={ {textDecoration: finish ? "line-through": "", color: finish ? "gray": ""}}>{note.text}</h2>
+            <button className="finish" onClick={toggleFinish}>finish</button>
             <button className="delete-btn" onClick={()=>deleteNote(note.id)}>X</button>
         </div>
     )
